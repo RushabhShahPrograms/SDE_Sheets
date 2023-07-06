efb452ddda45eed48378c8fb9e335eba25ef62fa
Rushabh Shah <rushabhshah122000@gmail.com>
2023-07-06 19:13:01 +0530

    new codes

---
+++ Striver_SDE/Array/11_remove_duplicates.py
@@ -0,0 +1,39 @@
+'''
+Problem Statement: Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+If there are k elements after removing the duplicates, then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Note: Return k after placing the final result in the first k slots of the array.
+
+Examples
+Example 1: 
+
+Input: arr[1,1,2,2,2,3,3]
+
+Output: arr[1,2,3,_,_,_,_]
+
+Explanation: Total number of unique elements are 3, i.e[1,2,3] and Therefore return 3 after assigning [1,2,3] in the beginning of the array.
+
+Example 2: 
+
+Input: arr[1,1,1,2,2,3,3,3,3,4,4]
+
+Output: arr[1,2,3,4,_,_,_,_,_,_,_]
+
+Explanation: Total number of unique elements are 4, i.e[1,2,3,4] and Therefore return 4 after assigning [1,2,3,4] in the beginning of the array.
+'''
+
+from typing import List
+def removeDuplicates(arr):
+    i=0
+    for j in range(1,len(arr)):
+        if arr[i] != arr[j]:
+            i += 1
+            arr[i] = arr[j]
+    return i+1
+
+arr = [1,1,2,2,2,3,3]
+k = removeDuplicates(arr)
+print("The array after removing duplicate elements is ")
+for i in range(k):
+    print(arr[i],end=" ")
